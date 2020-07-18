7aac2400d7c0f30109cd0a8ac531c1daadb7f712
sujanandan <62235113+sujanandan@users.noreply.github.com>
2020-07-18 09:17:41 +0530

    Update api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,22 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-// const mongoose = require('mongoose');
-
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 const ProductData=require('../models/Productdata');
-
-// const user = require('../models/user');
-
-// mongoose.connect(db,function(err){
-//     if(err){
-//         console.error("Error"+err);
-//     }
-//     else{
-//         console.log("Connected to mongodb");
-//     }
-// });
 
 router.get('/',(req,res) => {
     res.send("From Api");
